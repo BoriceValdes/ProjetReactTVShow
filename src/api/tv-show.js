7bc39d4ffe1a833d35622c39c969ac6e051e88a5
Noumedem <boricenoumedem4@gmail.com>
2024-10-17 19:29:19 +0100

    env file

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,17 +1,17 @@
 import axios from "axios"
-import { BASE_URL, API_KEY } from "../config"
+import { BASE_URL } from "../config"
 
 export class TVShowAPI {
     static async fetchPopulars(){
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY}`)
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY}}`)
         return response.data.results
     }
     static async fetchRecommendations(tvShowTd){
-        const response = await axios.get(`${BASE_URL}tv/${tvShowTd}/recommendations${API_KEY}`)
+        const response = await axios.get(`${BASE_URL}tv/${tvShowTd}/recommendations?api_key=${process.env.REACT_APP_API_KEY}}`)
         return response.data.results
     }
     static async fetchByTitle(title){
-        const response = await axios.get(`${BASE_URL}search/tv${API_KEY}&query=${title}`)
+        const response = await axios.get(`${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY}}&query=${title}`)
         return response.data.results
     }
 }
