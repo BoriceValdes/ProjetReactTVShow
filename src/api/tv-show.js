05e3ab208d737491f49583ef57bfbd62e6b14a87
Noumedem <boricenoumedem4@gmail.com>
2024-10-17 19:37:21 +0100

    modification de la class TVShow

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -3,15 +3,15 @@ import { BASE_URL } from "../config"
 
 export class TVShowAPI {
     static async fetchPopulars(){
-        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY}}`)
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY}`)
         return response.data.results
     }
     static async fetchRecommendations(tvShowTd){
-        const response = await axios.get(`${BASE_URL}tv/${tvShowTd}/recommendations?api_key=${process.env.REACT_APP_API_KEY}}`)
+        const response = await axios.get(`${BASE_URL}tv/${tvShowTd}/recommendations?api_key=${process.env.REACT_APP_API_KEY}`)
         return response.data.results
     }
     static async fetchByTitle(title){
-        const response = await axios.get(`${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY}}&query=${title}`)
+        const response = await axios.get(`${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${title}`)
         return response.data.results
     }
 }
